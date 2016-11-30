b1f6dc6665f34719a6317914939612da548e801d
remdp <remmdp@GMAIL.COM>
2016-11-30 20:27:18 +0200

    add media player

--- app/src/main/java/com/example/java/simpleplayer/MainActivity.java
+++ app/src/main/java/com/example/java/simpleplayer/MainActivity.java
@@ -1,27 +1,67 @@
 package com.example.java.simpleplayer;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Handler;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import Services.PlayBackService;
+import com.example.java.simpleplayer.services.PlayBackService;
 
 public class MainActivity extends AppCompatActivity {
+
+    private PlayBackService mService;
+    private boolean mBound = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Intent playbackIntent = new Intent(this, PlayBackService.class);
+        final Intent playbackIntent = PlayBackService.newInstance(this);
+        playbackIntent.setAction(PlayBackService.ACTION_PLAY);
         startService(playbackIntent);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                stopService(playbackIntent);
+                stopService(PlayBackService.newInstance(MainActivity.this));
             }
-        },500);
+        },2000);
+
+    };
+
+    private ServiceConnection mConnection  = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            PlayBackService.PlayBackBinder binder =
+                    (PlayBackService.PlayBackBinder) service;
+            mService = binder.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mBound = false;
+        }
+    };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent playBackIntent = PlayBackService.newInstance(this);
+        bindService(playBackIntent,mConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mBound){
+            unbindService(mConnection);
+            mBound = false;
+        }
     }
 }
---
+++ app/src/main/java/com/example/java/simpleplayer/Services/PlayBackService.java
@@ -0,0 +1,110 @@
+package com.example.java.simpleplayer.services;
+
+import android.app.Service;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.compat.BuildConfig;
+import android.util.Log;
+import android.widget.Toast;
+
+public class PlayBackService extends Service implements MediaPlayer.OnPreparedListener {
+
+    public static final String TAG = PlayBackService.class.getSimpleName();
+
+    private final IBinder mBinder = new PlayBackBinder();
+
+    public static final String ACTION_PLAY = BuildConfig.APPLICATION_ID + ".action.PLAY";
+
+    private MediaPlayer mMediaPlayer = null;
+
+    public PlayBackService() {
+    }
+
+    public static Intent newInstance(Context context){
+        return new Intent(context, PlayBackService.class);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Toast.makeText(this, "OnBind()", Toast.LENGTH_SHORT).show();
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mediaPlayer) {
+        mMediaPlayer.start();
+    }
+
+    public class PlayBackBinder extends Binder{
+        public PlayBackService getService(){
+            return PlayBackService.this;
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "OnCreate");
+        Toast.makeText(this, "onCreate()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "OnDestroy");
+        Toast.makeText(this, "onDestroy()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        try{
+        if (intent.getAction().equals(ACTION_PLAY)) {
+            mMediaPlayer = new MediaPlayer();
+            mMediaPlayer.setDataSource(this,getSongs());
+            mMediaPlayer.setOnPreparedListener(this);
+            mMediaPlayer.prepareAsync(); // prepare async to not block main thread
+        }}catch (Exception e){
+            e.printStackTrace();
+        }
+        return Service.START_STICKY;
+    }
+
+    private Uri getSongs(){
+        ContentResolver contentResolver = getContentResolver();
+        Uri uri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor cursor = contentResolver.query(uri, null, null, null, null);
+        if (cursor == null) {
+            // query failed, handle error.
+        } else if (!cursor.moveToFirst()) {
+            // no media on the device
+        } else {
+            int titleColumn = cursor.getColumnIndex(android.provider.MediaStore.Audio.Media.TITLE);
+            int idColumn = cursor.getColumnIndex(android.provider.MediaStore.Audio.Media._ID);
+           // do {
+            cursor.moveToNext();
+            cursor.moveToNext();
+            cursor.moveToNext();
+            cursor.move(50);
+                long thisId = cursor.getLong(idColumn);
+                String thisTitle = cursor.getString(titleColumn);
+                Uri contentUri = ContentUris.withAppendedId(
+                        android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, thisId);
+                return contentUri;
+                // ...process entry...
+          //  } while (cursor.moveToNext());
+        }
+        return uri;
+    }
+}
